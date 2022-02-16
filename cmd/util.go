1b2391abd710c836be1003c5cbe5d5ea7dd0e1a7
harryzhu <harry.chuh@gmail.com>
2022-02-16 23:55:57 +0800

    -

---
+++ cmd/util.go
@@ -0,0 +1,85 @@
+package cmd
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/disk"
+	"github.com/shirou/gopsutil/v3/host"
+	"github.com/shirou/gopsutil/v3/mem"
+)
+
+func init() {
+	Hinfo = DefaultHostinfo()
+	GetHost()
+	GetCPU()
+	GetMemory()
+	GetDisk()
+
+	json_hinfo, _ := json.Marshal(Hinfo)
+	fmt.Println("=======")
+	fmt.Println(string(json_hinfo))
+	ioutil.WriteFile("hinfo.json", json_hinfo, 0755)
+}
+
+type Hostinfo struct {
+	Id   string                 `json:"_id"`
+	Rev  string                 `json:"_rev"`
+	Data map[string]interface{} `json:"data"`
+}
+
+var Hinfo *Hostinfo
+
+func DefaultHostinfo() *Hostinfo {
+	hinfo := &Hostinfo{}
+	hinfo.Id = "pls-set-your-key"
+	data := make(map[string]interface{}, 10)
+	data["cpu"] = ""
+	hinfo.Data = data
+	return hinfo
+}
+
+func GetHost() {
+	h, _ := host.Info()
+	fmt.Println(h)
+
+	Hinfo.Id = h.Hostname
+	Hinfo.Data["host"] = h
+}
+
+func GetMemory() {
+	vm, _ := mem.VirtualMemory()
+	sm, _ := mem.SwapMemory()
+	Hinfo.Data["virtual_memory"] = vm
+	Hinfo.Data["swap_memory"] = sm
+	fmt.Println(vm)
+	fmt.Println(sm)
+}
+
+func GetCPU() {
+	c, _ := cpu.Info()
+	Hinfo.Data["cpu"] = c
+
+	fmt.Println(c)
+}
+
+func GetDisk() {
+	du := []*disk.UsageStat{}
+	partitions, _ := disk.Partitions(true)
+	for _, p := range partitions {
+		//fmt.Println(p)
+		if p.Mountpoint != "" {
+			pu, err := disk.Usage(p.Mountpoint)
+			if err != nil {
+				fmt.Println(err)
+			} else {
+				du = append(du, pu)
+			}
+
+		}
+	}
+	Hinfo.Data["disk"] = du
+	fmt.Println(du)
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module hostinfo
+
+go 1.17
+
+require (
+	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
+	github.com/power-devops/perfstat v0.0.0-20210106213030-5aafc221ea8c // indirect
+	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
+	github.com/shirou/gopsutil/v3 v3.22.1 // indirect
+	github.com/spf13/cobra v1.3.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/tklauser/go-sysconf v0.3.9 // indirect
+	github.com/tklauser/numcpus v0.3.0 // indirect
+	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sys v0.0.0-20220111092808-5a964db01320 // indirect
+)
